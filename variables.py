39bbc49ee1e9d9f7bee00b2614b700a32add9ea2
CR-h-a-n <Foksic2006@yandex.ru>
2022-06-16 11:39:42 +0300

    add modul 1

---
+++ variables.py
@@ -0,0 +1,19 @@
+# товар GILLETTE / Одноразовые Мужские Бритвы Blue2 Maximum, с 2 лезвиями в станке, плавающая головка
+
+product_name = 'GILLETTE. Blue2 Maximum'    # Наименование товара
+weight_g = 48   # Вес товара с упаковкой (г)
+price = 690.5   # Цена (р)
+quantity = 8   # Количество в упаковке (шт)
+property1 = 'Одноразовая мужская бритва'    # Свойство 1
+property2 = 'Плавающая головка, с углом наклона 40°, повторяет контуры лица'    # Свойство 2
+number_blades = 2   # Количество лезвий
+available = True    # Наличие
+
+print('Наименование товара:', product_name, type(product_name))
+print('Вес товара с упаковкой (г):', weight_g, type(weight_g))
+print('Цена (р):', price, type(price))
+print('Количество в упаковке (шт):', quantity, type(quantity))
+print('Свойство:', property1, type(property1))
+print('Свойство:', property2, type(property2))
+print('Количество лезвий:', number_blades, type(number_blades))
+print('Наличие:', available, type(available))
